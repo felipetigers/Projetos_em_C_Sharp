1efdf8775e8478a64c7780e0de8a2bb521f70d60
Felipe Vale <105613059+felipetigers@users.noreply.github.com>
2022-06-09 15:39:54 -0300

    first commit
    programa calcula o perímetro de um quadrado, utilizando uma classe

---
+++ Quadrado/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBasico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UtilizarQuadrado();
+            Console.ReadKey();      //permite pausar a tela para ver os dados de saida
+        }
+
+        static void UtilizarQuadrado()
+        {
+            float l, p;
+            Quadrado q = new Quadrado();  //cria-se um objeto e o instancia na memoria heap ao mesmo tempo
+            
+            //console.write usado pra mostrar informacaoes na tela
+            Console.Write("Informe o valor do lado: ");
+            l = float.Parse(Console.ReadLine());    //atribui o valor recebido à variável "l"
+            q.Lado = l;
+            p = q.Perimetro();
+            
+            Console.WriteLine("O lado é " + l);            
+            Console.WriteLine("O perimetro do quadrado eh " + p);
+            
+            
+
+        }
+    }
+}
---
+++ Quadrado/Quadrado.cs
@@ -0,0 +1,19 @@
+﻿namespace CSharpBasico
+{
+    class Quadrado  //classe quadrado
+    {
+        float _Lado;    //underline para distinguir o atributo com a propriedade
+
+        public float Lado       //Lado é a propriedade do atributo _Lado
+        {
+            set { _Lado = value; }      //setter
+            get { return _Lado; }        //getter
+        }
+
+        public float Perimetro()    //visibilidade pública para que o método seja acessível fora da classe
+        {
+            return 4 * Lado;    
+        }
+
+    }
+}
